9528bdb76de6436f49b2c89767aee3ce3d449bb8
pdalelopez <pdalelopez@users.noreply.github.com>
2020-03-24 17:31:16 +0100

    user and video classes created

---
+++ Videos/src/User.java
@@ -0,0 +1,18 @@
+
+public class User 
+{	
+	String UserName,Name, LastName,PassWord;
+	
+	public User(String userName, String name,String lastName,String passWord) 
+	{
+		this.UserName = userName;
+		this.Name = name;
+		this.LastName = lastName;
+		this.PassWord = passWord;
+	}
+	
+	
+	
+	
+
+}
---
+++ Videos/src/Video.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Video 
+{
+	String Url,Tytle;
+	ArrayList<String> Tags;
+	
+	public Video (String url, String tytle, ArrayList<String> tags) 
+	{
+		this.Url = url;
+		this.Tytle = tytle;
+		this.Tags = tags;
+	
+
+	}
+}
