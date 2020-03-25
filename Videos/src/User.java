fcbd6c452169a30bd504c00b53a778951460f923
pdalelopez <pdalelopez@users.noreply.github.com>
2020-03-25 17:18:47 +0100

    added toString Override method, setters and getters and blank's user constructor

--- Videos/src/User.java
+++ Videos/src/User.java
@@ -4,6 +4,11 @@ public class User
 	
 	private String UserName,Name, LastName,PassWord;
 	
+	public User() 
+	{
+		
+	}
+	
 	public User(String userName, String name,String lastName,String passWord) 
 	{
 		this.UserName = userName;
@@ -12,60 +17,50 @@ public class User
 		this.PassWord = passWord;
 	}
 	
-	/**
-	 * @return the userName
-	 */
-	public String getUserName() {
+	public String getUserName() 
+	{
 		return UserName;
 	}
 
-	/**
-	 * @param userName the userName to set
-	 */
-	public void setUserName(String userName) {
+	public void setUserName(String userName) 
+	{
 		UserName = userName;
 	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
+	public String getName() 
+	{
 		return Name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
+	public void setName(String name) 
+	{
 		Name = name;
 	}
 
-	/**
-	 * @return the lastName
-	 */
-	public String getLastName() {
+	public String getLastName() 
+	{
 		return LastName;
 	}
 
-	/**
-	 * @param lastName the lastName to set
-	 */
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName) 
+	{
 		LastName = lastName;
 	}
 
-	/**
-	 * @return the passWord
-	 */
-	public String getPassWord() {
+	public String getPassWord() 
+	{
 		return PassWord;
 	}
 
-	/**
-	 * @param passWord the passWord to set
-	 */
-	public void setPassWord(String passWord) {
+	public void setPassWord(String passWord) 
+	{
 		PassWord = passWord;
+	}
+
+	@Override
+	public String toString() {
+		return "User [UserName=" + getUserName() + ", Name=" + getName() + ", LastName=" + getLastName() + ", PassWord=" + getPassWord()
+				+ "]";
 	}
 
 	
