38efa4a0d33121d9ffa0959ac2e0c013007a5955
pdalelopez <pdalelopez@users.noreply.github.com>
2020-03-25 18:42:34 +0100

    cheking the user with the user'list work

--- Videos/src/Main.java
+++ Videos/src/Main.java
@@ -3,49 +3,22 @@ import java.util.Scanner;
 
 public class Main 
 {
-
+	static boolean isUserExist;
+	
 	public static void main(String[] args) 
 	{
-		ArrayList<User> userList;
-		User logUser;
+		ArrayList<User> usersList;
 		String logUserName,logPassWord;
-		boolean isUserExist,isOtherUser;
-		int counter;
+		User logUser;
 		Scanner input;
 		
+		
 		input = new Scanner (System.in);
+		isUserExist = true;
 		
 		//adding user to array list using "createUser" method
-		userList = new ArrayList<User>();
-		isOtherUser = true;
-		counter=0;
-		
-		do 
-		{
-		System.out.println("Enter a new user");	
-		userList.add(counter,createUser(input));		
-		counter++; 
-		System.out.println("Do you want to add other user?");
-		String userAnswer = input.next().toLowerCase();
-		boolean isAnswerCorrect = false;
-			while (!isAnswerCorrect) 
-			{
-				if(userAnswer.equals("si")) 
-				{
-					isAnswerCorrect=true;
-					
-				}else if(userAnswer.equals("no"))
-				 {
-					isAnswerCorrect=true;
-					isOtherUser=false;
-				 }else 
-				  {
-					System.out.println("ERROR! Answer should be SI or NO");
-					System.out.println("Do you want to add other user?");
-					userAnswer = input.next().toLowerCase();
-				  }	
-			}
-		} while (isOtherUser);
+		usersList = new ArrayList<User>();
+		usersList = createUserList(input);
 		
 		// Entering user to logging
 		System.out.println("Enter your \"username\"");
@@ -53,26 +26,97 @@ public class Main
 		System.out.println("Enter your \"password\"");
 		logPassWord = input.next();
 		
-		// checking is User exist in the list
-		for(int i =0 ; i< userList.size(); i++) 
+		//checking user
+		logUser = checkUser(logUserName,logPassWord,usersList);
+		
+		
+		//Running the application
+		
+		
+		if(isUserExist) 
 		{
-			if (userList.get(i).getUserName().equals(logUserName)) 
+			System.out.println("Welcome "+logUser.getName());
+			System.out.println("Running");
+			System.out.println("Running");
+			System.out.println("Running");
+			System.out.println("Running");
+			System.out.println("Finish");
+		}else 
+		 {
+			System.out.println("this user do not exist");
+			System.out.println("Fatal ERROR");
+		 }
+		
+		
+
+			
+	}
+	
+		// method checks if User exist in the list
+	
+	public static User checkUser(String logUserName,String logPassWord,ArrayList<User> usersList ) {
+		
+		User logUser = new User();
+		
+		for(int i =0 ; i< usersList.size(); i++) 
+		{
+			if (usersList.get(i).getUserName().equals(logUserName)) 
 			{
-				if(userList.get(i).getPassWord().equals(logPassWord)) 
+				if(usersList.get(i).getPassWord().equals(logPassWord)) 
 				{
-					logUser = userList.get(i);
-					i=userList.size();
-					System.out.println("Welcome "+logUser.getName());
-					isUserExist = true;
+					logUser = usersList.get(i);
+					i=usersList.size();
 				}else 
 				 {
-					System.out.println("this user do not exist");
 					isUserExist = false;
 				 }
-				
 			}
-		}
 			
+		}
+		return logUser;
+	}
+	
+	//create user'list
+	
+	public static ArrayList<User> createUserList (Scanner input)
+	{
+		ArrayList<User> usersList = new ArrayList<User>();
+		boolean isOtherUser,isRightAnswer;
+		String userAnswer;
+		
+		isOtherUser = true;
+		do 
+		{
+		System.out.println("Enter a new user");	
+		usersList.add(createUser(input));		 
+		
+		System.out.println("Do you want to add other user?");
+		userAnswer = input.next().toLowerCase();
+		
+		isRightAnswer = false;
+			do
+			{
+				if(userAnswer.equals("si"))
+				{
+					isRightAnswer=true;
+					
+				}else if(userAnswer.equals("no"))
+				 {
+					isRightAnswer=true;
+					isOtherUser = false;
+					
+					
+				 }else 
+				  {
+					System.out.println("ERROR! Answer should be SI or NO");
+					System.out.println("Do you want to add other user?");
+					userAnswer = input.next().toLowerCase();
+				  }	
+			} while (!isRightAnswer);
+		
+		} while (isOtherUser);
+		
+		return usersList;
 	}
 	
 	// create new user
@@ -89,6 +133,8 @@ public class Main
 		System.out.println("passWord");
 		passWord = input.next();
 		user= new User(userName,name,lastName,passWord);
+		
+		System.out.println(user);
 		
 		return user;
 	}
